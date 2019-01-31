48dec4b1daa6c5609f15d646e9aec7119900f163
NilsHasNoGithub <niolopa@gmail.com>
2019-01-31 14:23:06 +0100

    cghvk

--- Assignment1/src/assignment1/Main.java
+++ Assignment1/src/assignment1/Main.java
@@ -1,10 +1,10 @@
 package assignment1;
 
-
 import static assignment1.GroupManager.maintainGroup;
 import static assignment1.GroupManager.promptStudents;
 
 import nl.nils.console.Console;
+import nl.nils.utilities.Utilities;
 
 /**
  * @author Nils Golembiewski (s1019649)
@@ -14,14 +14,14 @@ import nl.nils.console.Console;
 public class Main {
 
     public static void main(String[] args) {
-        Console console=new Console();
-        console.setSize(800, 400);
-        Student[] students = promptStudents(console);
-        maintainGroup(students,console);
-        console.linePrint("Bye!");
-        console.waitForKeyPress("Press any key to exit...");
-        console.close();
+        try (Console console = new Console()) {
+            console.setSize(800, 400);
+            console.setFeedbackIndicator(Utilities.seperationLine(50) + "\n", "\n" + Utilities.seperationLine(50));
+            Student[] students = promptStudents(console);
+            maintainGroup(students, console);
+            console.linePrint("Bye!");
+            console.waitForKeyPress("Press any key to exit...");
+        }
     }
 
-    
 }
